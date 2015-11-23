cd36df11f97b0569a8b2c584d5c925a9a9561350
Dave Andreoli <dave@gurumeditation.it>
2015-11-23 23:51:59 +0100

    More upnp work

--- upnp/eupnp.py
+++ upnp/eupnp.py
@@ -1,15 +1,51 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
+import os
 import struct
 import socket
 
-import urllib2
+# import urllib2
+from xml.etree import ElementTree
+# import xml.dom.minidom
 
 
 from efl import ecore, ecore_con
 from efl import elementary as elm
+
+
+
+#-------------
+from collections import defaultdict
+
+def strip_ns(s):
+    if s.startswith('{') and '}' in s:
+        return s.split('}')[1]
+    return s
+
+def etree_to_dict2(t):
+
+    tag = strip_ns(t.tag)
+
+    d = {tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict2, children):
+            for k, v in dc.iteritems():
+                dd[strip_ns(k)].append(v)
+        d = {tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
+    if t.attrib:
+        d[tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[tag]['#text'] = text
+        else:
+            d[tag] = text
+    return d
+#-------------
 
 
 class UrlSimple(ecore_con.Url):
@@ -34,28 +70,165 @@ class UrlSimple(ecore_con.Url):
     def _data_cb(self, event):
         self.received_data.append(event.data)
 
+#-------------
 
-class UPnP_Host(object):
-    def __init__(self, uuid, xml_url):
-        print("VALID!!! " + uuid)
-        self.uuid = uuid
+class UPnP_Service(object):
+    def __init__(self, parent_host, service_id, service_type, xml_url, control_url, event_url):
+        self.parent_host = parent_host
+        self.service_id = service_id
+        self.service_type = service_type
         self.xml_url = xml_url
+        self.control_url = control_url
+        self.event_url = event_url
 
         self.request_xml()
 
-    # XML stuff...
-    def _xml_complete(self, url, status, data):
-        print("@@@@@@@@@@@")
-        print(url)
-        print(status)
-        print(data)
+    def __str__(self):
+        return "<UPnP_Service '{}' at '{}'>".format(
+                self.service_id, self.parent_host.friendly_name)
 
+    # XML stuff...
     def request_xml(self):
+        print("REQUEST SERVICE XML " + self.xml_url)
         u = UrlSimple(self.xml_url, self._xml_complete)
         u.additional_header_add('USER-AGENT', 'UPnP/2.0')
         u.additional_header_add('CONTENT-TYPE','text/xml; charset="utf-8"')
         u.get()
 
+    def _xml_complete(self, url, status, data):
+        print("SERVICE DONE " + str(status) + str(url if status != 200 else ''))
+        # print(data)
+        # print("========================================")
+        # pass
+
+
+class UPnP_Host(object):
+    def __init__(self, uuid, xml_url):
+        print("VALID!!! " + uuid)
+        self.uuid = uuid
+        self._xml_url = xml_url
+        self._base_url = ''
+
+        self.device_info = {} # raw info from xml
+        self.services = {}    # key:serviceId val:UPnP_Service() instance
+
+        self.request_xml()
+
+    def __str__(self):
+        return "<UPnP_Host '{}' of type '{}'>".format(
+                self.friendly_name, self.device_type)
+
+    @property
+    def UDN(self):
+        return self.device_info.get('UDN')
+
+    @property
+    def device_type(self):
+        return self.device_info.get('deviceType')
+
+    @property
+    def friendly_name(self):
+        return self.device_info.get('friendlyName')
+
+    @property
+    def manufacturer(self):
+        return self.device_info.get('manufacturer')
+
+    @property
+    def manufacturer_url(self):
+        return self.device_info.get('manufacturerURL')
+
+    @property
+    def model_name(self):
+        return self.device_info.get('modelName')
+
+    @property
+    def model_number(self):
+        return self.device_info.get('modelNumber')
+
+    @property
+    def model_url(self):
+        return self.device_info.get('modelURL')
+
+    @property
+    def serial_number(self):
+        return self.device_info.get('serialNumber')
+
+    # XML stuff...
+    def request_xml(self):
+        print("REQUEST DEVICE XML " + self._xml_url)
+        u = UrlSimple(self._xml_url, self._xml_complete)
+        u.additional_header_add('USER-AGENT', 'UPnP/2.0')
+        u.additional_header_add('CONTENT-TYPE','text/xml; charset="utf-8"')
+        u.get()
+
+    def _xml_complete(self, url, status, data):
+
+        if status != 200:
+            return
+
+        from pprint import pprint
+            
+        print("@@@@@@@@@@@")
+        # print(url)
+        # print(status)
+        # print(data)
+        # print("---")
+
+        ns = '{urn:schemas-upnp-org:device-1-0}'
+        root = ElementTree.fromstring(data)
+        # print(root)
+
+        # honor the old URLBase spec (or use the xml base path)
+        node = root.find(ns+'URLBase')
+        if node is not None:
+            self._base_url = node.text
+        else:
+            self._base_url = os.path.dirname(self._xml_url)
+
+        # build a dict of the whole <device> tag
+        device = root.find(ns+'device')
+        device_info = etree_to_dict2(device)
+        device_info = device_info['device'] # :/
+
+        # handle services
+        lis = device_info['serviceList']['service']
+        if not isinstance(lis, list):
+            lis = [lis]
+
+        for service in lis:
+            service_id = service['serviceId']
+            if not service_id in self.services:
+                s = UPnP_Service(self, service_id,
+                                 service['serviceType'],
+                                 self._fix_base_url(service['SCPDURL']),
+                                 self._fix_base_url(service['controlURL']),
+                                 self._fix_base_url(service['eventSubURL']))
+                self.services[service_id] = s
+        del device_info['serviceList']
+
+        # TODO handle icons
+        
+
+        self.device_info = device_info
+
+
+        # debug...
+        print("--- COMPLETED HOST:")
+        print(self)
+        # pprint(self.device_info)
+        for service in self.services:
+            print(self.services[service])
+
+
+        print("---")
+
+    def _fix_base_url(self, url):
+        if url.startswith(self._base_url):
+            return url
+        if not self._base_url.endswith('/') and not url.startswith('/'):
+            return self._base_url + '/' + url
+        return self._base_url + url
 
 class UPnP_Network(object):
     def __init__(self):
